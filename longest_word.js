9ae0614af7fd58b2bfedc13bb2afb41e12d81f2c
toowan <mail@toowan.me>
2017-01-13 17:00:32 -0800

    FCC: Longest Word in JS

--- longest_word.js
+++ longest_word.js
@@ -1,86 +1,79 @@
-// Return the length of the longest word in the provided sentence. 
-// Your response should be a number.
+// Title Case a Sentence
+
+// Return the provided string with the first letter of each word capitalized. 
+// Make sure the rest of the word is in lower case.
+// For the purpose of this exercise, you should also capitalize connecting words like "the" and "of".
 
 
+// Using .map and .replace method:
 
-function findLongestWord(str) {
+function titleCase(str) {
+  // Step 1. Lowercase the string
+  str = str.toLowerCase();
   
-  // split string into an array of strings
-  str = str.split(' ');
-  
-  // If length of current word is greater than longestWord, replace longestWord with current word.  
-  var longestWord = 0; 
-  for (var i = 0; i < str.length; i++) { 
-    if (str[i].length > longestWord) {
-      longestWord = str[i].length; 
-    }
-  }
- 
-  return longestWord; 
-  
+  // Step 2. Split the string into an array of strings
+  str = str.split(' '); 
+         
+  // Step 3. Map over the array
+  str = str.map(function(word) {
+    return word.replace(word[0], word[0].toUpperCase()); 
+    // => "little".replace("l", "L");
+});
+
+ // Step 4. Return the output
+ return str.join(' '); 
 }
 
 
-// TEST CASES
-
-findLongestWord("The quick brown fox jumped over the lazy dog");   // => 6
 
 
+// Using .map and .slice method:
 
-
-// 'reduce' Method: 
-
-function findLongestWord(str) {
-  // Step 1. Split the string into an array of strings
-  var strSplit = str.split(' ');
-
-  // Step 2. Use the reduce method
-  var longestWord = strSplit.reduce(function(longest, currentWord) {
-    if(currentWord.length > longest.length)
-       return currentWord;
-    else
-       return longest;
-  }, "");
+function titleCase(str) {
+  // Step 1. Lowercase the string
+  str = str.toLowerCase();
   
-  /* Reduce process
-  currentWord      longest       currentWord.length     longest.length    if(currentWord.length > longest.length)?   var longestWord
-  "The"             ""                  3                     0                              yes                          "The"
-  "quick"           "The"               5                     3                              yes                         "quick"
-  "brown"           "quick"             5                     5                              no                          "quick"
-  "fox"             "quick"             3                     5                              no                          "quick"
-  "jumped"          "quick"             6                     5                              yes                         "jumped"
-  "over"            "jumped"            4                     6                              no                          "jumped"
-  "the"             "jumped"            3                     6                              no                          "jumped"
-  "lazy"            "jumped"            4                     6                              no                          "jumped"
-  "dog"             "jumped"            3                     6                              no                          "jumped"
-  */
-  
+  // Step 2. Split the string into an array of strings
+  str = str.split(' '); 
+         
+  // Step 3. Map over the array
+  str = str.map(function(word) {
+    return (word.charAt(0).toUpperCase() + word.slice(1));  
+    // "little".charAt(0).toUpperCase() + "little".slice(1) =>  "L"  + "ittle";
 
-  return longestWord.length;
+});
+
+ // Step 4. Return the output
+ return str.join(' '); 
 }
 
- 
- // 'sort' Method:
 
- function findLongestWord(str) {
-  // Step 1. Split the string into an array of strings
-  var strSplit = str.split(' ');
+
+// Using FOR loop and .slice method:
+
+function titleCase(str) {
+  // Step 1. Lowercase the string
+  str = str.toLowerCase();
   
-  // Step 2. Sort the elements in the array from biggest to smallest
-  var longestWord = strSplit.sort(function(a, b) { 
-    return b.length - a.length;
-  });
-  /* Sorting process
-    a           b            b.length     a.length     var longestWord
-  "The"      "quick"            5            3         ["quick", "The"]
-  "quick"    "brown"            5            5         ["quick", "brown", "The"]  
-  "brown"    "fox"              3            5         ["quick", "brown", "The", "fox"]
-  "fox"      "jumped"           6            3         ["jumped", quick", "brown", "The", "fox"]
-  "jumped"   "over"             4            6         ["jumped", quick", "brown", "over", "The", "fox"]
-  "over"     "the"              3            4         ["jumped", quick", "brown", "over", "The", "fox", "the"]
-  "the"      "lazy"             4            3         ["jumped", quick", "brown", "over", "lazy", "The", "fox", "the"]
-  "lazy"     "dog"              3            4         ["jumped", quick", "brown", "over", "lazy", "The", "fox", "the", "dog"]
-  */
-  
-  // Step 3. Return the length of the first element (because it's the biggest)
-  return longestWord[0].length; 
+  // Step 2. Split the string into an array of strings
+  str = str.split(' '); 
+         
+  // Step 3. Iterate over the array with a FOR loop
+  for (var i = 0, i < str.length; i++) {
+    str[i] = str[i].charAt(0).toUpperCase() + word.slice(1);  
+    // "little".charAt(0).toUpperCase() + "little".slice(1) =>  "L"  + "ittle";
+}
+
+ // Step 4. Return the output
+ return str.join(' '); 
+}
+
+
+
+
+
+// TEST CASES:
+
+titleCase("I'm a little tea pot");
+titleCase("sHoRt AnD sToUt"); 
+titleCase("HERE IS MY HANDLE HERE IS MY SPOUT"); 

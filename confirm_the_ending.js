47193a53c711d24a1f672809f51f05d1869c150b
toowan <mail@toowan.me>
2017-01-15 19:16:45 -0800

    FCC: Confirm the Ending in JS

---
+++ confirm_the_ending.js
@@ -0,0 +1,28 @@
+// Confirm the Ending
+
+function confirmEnding(str, target) {
+
+  // Specify the start index of the char to not include in the returned substring
+  var startIndex = (str.length - target.length);  // 11 - 4 = 7
+  // Specify the end index of the char to NOT include in the returned substring
+  var endIndex = (str.length);  // 11 - 1 = 10
+
+  // Slice out the end substring that matches the length of the target
+  var endSubstring = str.substring(startIndex, endIndex);
+
+  // If the target matches the end substring, return true
+  if target === endSubstring {
+    return true
+  };
+}
+
+
+// TEST CASES
+
+confirmEnding("Bastian", "n") // => true.
+confirmEnding("Connor", "n") // => false.
+confirmEnding("Walking on water and developing software from a specification are easy if both are frozen", "specification") // => false.
+confirmEnding("He has to give me a new name", "name") // => true.
+confirmEnding("Open sesame", "same") // => true.
+confirmEnding("Open sesame", "pen") // => false.
+confirmEnding("If you want to save our world, you must hurry. We dont know how much longer we can withstand the nothing", "mountain") // => false.

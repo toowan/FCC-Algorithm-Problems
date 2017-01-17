4077f5ce3bf6689fd1a6ffb18684a6fdd224bbce
toowan <mail@toowan.me>
2017-01-17 00:24:04 -0800

    FCC: Repeat A String in JS

---
+++ repeat_a_string.js
@@ -0,0 +1,19 @@
+// Repeat A String
+// Repeat a given string (first argument) num times (second argument). Return an empty string if num is not a positive number.
+
+// Using .repeat method
+function repeatStringNumTimes(str, num) {
+  // repeat after me
+  if (num > 0) {
+    return str.repeat(num); 
+  } else {
+    return ""; 
+  }
+}
+
+// Test Cases:
+
+repeatStringNumTimes("abc", 3) // => "abcabcabc"
+repeatStringNumTimes("*", 3) // => "***"
+repeatStringNumTimes("abc", 4) // => "abcabcabcabc" 
+repeatStringNumTimes("abc", -2) // => ""

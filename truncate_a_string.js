939e2ab429690b94bf60bc8bbf5b9b1256b68de7
toowan <mail@toowan.me>
2017-01-17 12:52:05 -0800

    FCC: Truncate a String in JS

--- truncate_a_string.js
+++ truncate_a_string.js
@@ -1,9 +1,7 @@
 // TRUNCATE A STRING
 
 /* Truncate a string (first argument) if it is longer than the given maximum string length (second argument). Return the truncated string with a ... ending.
-
 Note that inserting the three dots to the end will add to the string length.
-
 However, if the given maximum string length num is less than or equal to 3, then the addition of the three dots does not add to the string length in determining the truncated string. */ 
 
 
@@ -12,7 +10,7 @@ However, if the given maximum string length num is less than or equal to 3, then
 function truncateString(str, num) {
   // Truncate string to its max length if its length is greater than num
   if (str.length > num) {
-  	// If num is less than or equal to three, count the dots towards the max length
+    // If num is less than or equal to three, count the dots towards the max length
     if (num <= 3) {
       return str.slice(0, num) + "...";
     }  
@@ -37,3 +35,4 @@ truncateString("A-", 1) // => "A..."
 truncateString("Absolutely Longer", 2) // => "Ab..."
 truncateString("A-tisket a-tasket A green and yellow basket", "A-tisket a-tasket A green and yellow basket".length // => "A-tisket a-tasket A green and yellow basket"
 truncateString("A-tisket a-tasket A green and yellow basket", "A-tisket a-tasket A green and yellow basket".length + 2) // =>  "A-tisket a-tasket A green and yellow basket"
+

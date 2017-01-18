c44c54ea2a11443eff836fb31458a3a7c9de0f75
toowan <mail@toowan.me>
2017-01-17 17:32:02 -0800

    FCC: Mutations in JS

--- mutations.js
+++ mutations.js
@@ -1,21 +1,17 @@
-// MUTATIONS 
+// FALSY BOUNCER
 
 /* 
 
-Return true if the string in the first element of the array contains all of the letters of the string in the second element of the array.
+Remove all falsy values from an array.
 
-For example, ["hello", "Hello"], should return true because all of the letters in the second string are present in the first, ignoring case.
-
-The arguments ["hello", "hey"] should return false because the string "hello" does not contain a "y".
-
-Lastly, ["Alien", "line"], should return true because all of the letters in "line" are present in "Alien".
+Falsy values in JavaScript are false, null, 0, "", undefined, and NaN.
 
 */ 
 
 
 
 
-// Using .slice method
+// Using .indexOf() and .toLowerCase() methods
 
 
 function mutation(arr) {      
